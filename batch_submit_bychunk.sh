4b5d6b34c49f9b3a9d6d3441073a7415fe806cb3
Joe Ammatelli <53061783+j-amma@users.noreply.github.com>
2022-08-16 13:59:45 -0600

    Add files via upload

---
+++ batch_submit_bychunk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Specift directory to store file segments
+SEGMENT_DIR=./input_segments/
+mkdir -p $SEGMENT_DIR
+
+# define prefix for segment names
+SEGMENT_PREFIX=segment_
+
+# Specift number of nodes (number of file segments to generate)
+NUM_NODES=3
+
+# Create new file segment for each node
+NUM_LINES=$(wc -l < $1)
+LINES_PER_FILE=$(($NUM_LINES / $NUM_NODES))
+
+split -l $LINES_PER_FILE $1 $SEGMENT_DIR$SEGMENT_PREFIX
+
+# Submit job for each node
+for FILE in $SEGMENT_DIR*; 
+do 
+    # submit NCAR Bash script
+    bash treesway_submit_cheyenne.sh $FILE
+done
